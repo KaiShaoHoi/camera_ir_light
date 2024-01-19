59df9d537884d2b0f9e6bf9ff09994da0f54d9f2
kai-shao_ottoint <kai.shao@ottoint.com>
2024-01-19 09:22:54 +0100

    Fix logging error

--- custom_components/camera_ir_light/sensor.py
+++ custom_components/camera_ir_light/sensor.py
@@ -1,13 +1,17 @@
-import asyncio
+import logging
+from datetime import timedelta
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.const import STATE_ON, STATE_OFF
 from homeassistant.helpers.entity import Entity
 from .const import DOMAIN, CONF_HOST, CONF_POLLING_INTERVAL
 
+_LOGGER = logging.getLogger(__name__)
+SCAN_INTERVAL = timedelta(seconds=60)  # 默认轮询间隔，可以根据需要调整
+
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the camera_ir_light sensor platform."""
     host = config.get(CONF_HOST)
-    polling_interval = config.get(CONF_POLLING_INTERVAL, 60)
+    polling_interval = config.get(CONF_POLLING_INTERVAL, SCAN_INTERVAL.total_seconds())
 
     async_add_entities([IRLightSensor(hass, host, polling_interval)], True)
 
@@ -32,8 +36,18 @@ class IRLightSensor(Entity):
         """Return the state of the sensor."""
         return self._state
 
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return True
+
+    @property
+    def scan_interval(self):
+        """Return the polling interval for the sensor."""
+        return timedelta(seconds=self._polling_interval)
+
     async def async_update(self):
-        """Update the sensor."""
+        """Fetch new state data for the sensor."""
         try:
             color_flag = False
             blackwhite_flag = False
@@ -46,9 +60,8 @@ class IRLightSensor(Entity):
                         color_flag = True
                     elif "display switch(color -> blackwhite)." in line:
                         blackwhite_flag = True
-                        break  # 一旦匹配到 "display switch(color -> blackwhite)." 就停止遍历
+                        break
 
-            # 根据标志位设置 self._state
             if blackwhite_flag:
                 self._state = STATE_ON
             elif color_flag:
@@ -56,10 +69,9 @@ class IRLightSensor(Entity):
             else:
                 self._state = STATE_ON
 
-        except Exception as e:
+        except asyncio.TimeoutError:
+            _LOGGER.warning(f"Timeout error occurred while updating sensor state for {self.name}")
             self._state = STATE_OFF
-            # 在实际使用中，可以记录日志或者通过其他途径报告错误
-            _LOGGER.error(f"Error updating sensor state: {e}")
-
-        # 等待指定的轮询间隔时间
-        await asyncio.sleep(self._polling_interval)
+        except Exception as e:
+            _LOGGER.error(f"Error updating sensor state for {self.name}: {e}")
+            self._state = STATE_OFF
